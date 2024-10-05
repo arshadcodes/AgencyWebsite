aecc39d2580fe803f18e40c762f82a6e33c63452
Mic <sassom2112@gmail.com>
2024-10-05 11:10:28 -1000

    Fixed the three.js scene alignment to center and improve mobile display of circle-withoutBG.png

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -14,14 +14,15 @@ const Main = () => {
             </div>
 
 
-            <div className="flex justify-center mt-10">
-                <div className="w-[700px] h-[700px] rounded-full bg-[radial-gradient(closest-side,_rgba(0,0,0,0.5),_rgba(255,255,255,1))] relative">
-                    <img src="circle-withoutBG.png" alt="text" className="absolute left-[-100px] w-[700px]" />
-                    <div className="absolute w-[500px] h-[600px] right-[-150px]">
-                        <RocketCanvas />
+            <div className="flex justify-center mt-10"> 
+                <div className="w-full max-w-[700px] h-auto aspect-square rounded-full bg-[radial-gradient(closest-side,_rgba(0,0,0,0.5),_rgba(255,255,255,1))] relative">
+                    <img src="circle-withoutBG.png" alt="text" className="absolute left-0 w-full h-full" />
+                    <div className="absolute w-full h-auto inset-0">
+                    <RocketCanvas />
                     </div>
                 </div>
             </div>
+
 
             <div className="flex flex-col mx-24 mt-24">
                 <div className="text-sm">
