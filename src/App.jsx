2443457efb3c1fa1726520a897d6760546f77caf
Aditya Kumar Mishra <105539123+adityakmrmishra@users.noreply.github.com>
2024-10-07 19:37:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { div } from "framer-motion/client"
 import Footer from "./components/footer"
 import HowThingsWork from "./components/how things work"
 import Main from "./components/main"
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import OurWork from "./components/our work"
 
 function App() {
