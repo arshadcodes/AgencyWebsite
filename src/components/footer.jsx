fa4598c4b2cb587a9785f3d8e5775357f31b16c5
Clyde Forland <122248042+clydehenry3@users.noreply.github.com>
2024-10-07 13:20:12 -0500

    New responsive footer with adjusted top and bottom padding.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,15 +8,15 @@ const links = [
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200  px-36 dark:text-white dark:bg-gray-900">
-      <div className="grid grid-cols-2 gap-4 py-24 items-center">
+    <footer className="bg-gray-200 px-8 sm:px-12 md:px-36 dark:text-white dark:bg-gray-900">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-16 items-center">
         <div>
           <img
             src="Logo-withoutBG.png"
             alt="logo"
             className="w-24 dark:invert"
           />
-          <p className="text-lg pr-36">
+          <p className="text-lg md:pr-36">
             We&#39;re a software agency on a mission to bridge a gap between
             Marketplace Businesses and Innovation through custom software and
             automations.
@@ -26,7 +26,7 @@ const Footer = () => {
           <i className="fa-brands fa-square-instagram text-4xl"></i>
         </div>
         <div>
-          <ul className="list-none pl-36">
+          <ul className="list-none md:pl-36">
             {links.map((link) => (
               <li key={link.name}>
                 <a href="#" className="">
@@ -37,9 +37,9 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <div className="flex justify-between pb-4">
+      <div className="flex flex-col sm:flex-row justify-between pb-4">
         <div>All rights reserved to Egma</div>
-        <div>
+        <div className="mt-2 sm:mt-0">
           <a href="#" className="mr-12">
             Privacy Policy
           </a>
